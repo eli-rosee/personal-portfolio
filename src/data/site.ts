35a6221d18fb42118839fb42a56fa3ae4d1d27bf
eli-rosee <eli.j.rose.04@gmail.com>
2026-09-26 09:01:20 -0500

    Update tagline

--- src/data/site.ts
+++ src/data/site.ts
@@ -2,7 +2,7 @@ export const site = {
 	name: "Eli Rose",
 	callsign: "Station Home-01",
 	tagline:
-		"Master's Student. Aspiring Embedded Systems Software Engineer.",
+		"Master's Student at Georgia Tech. Aspiring Robotics Software Engineer.",
 	resume: "/resume.pdf",
 	github: "https://github.com/eli-rosee",
 	email: "eli.j.rose.04@gmail.com",
